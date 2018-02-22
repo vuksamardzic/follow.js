a27bcdf79686d0ef94991d1ac463e028b7e755c7
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-22 09:07:51 +0100

    attach listener & merge options for lib

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-let defaults = {
-    selector: '.follow-el',
-    areas: 8
-};
+import { defaults } from './models/defaults.model';
+import { followEl } from './services/cache-dom.service';
+import { mouseMoveHandler } from './events/mousemove.event';
+import { mergeOptions } from './services/merge-options.service';
 
-export let init = ( options ) => {
-    if ( typeof options === 'string' ) {
-        defaults.selector = options
-        console.log( defaults );
-    } else if ( typeof options === 'object' ) {
-        console.log( Object.assign(defaults, options) );
-    }
+export let init = (options) => {
+    mergeOptions(options);
+    document.addEventListener('mousemove', mouseMoveHandler);
 }
