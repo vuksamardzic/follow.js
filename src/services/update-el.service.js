761936aabdcd47eab30cb48122fec68b0c41b8a8
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-26 21:06:21 +0100

    separated logic when mouse is over element

--- src/services/update-el.service.js
+++ src/services/update-el.service.js
@@ -1,11 +1,23 @@
 import { getCenterPoint } from "./el-center.service";
 import { defaults } from "../models/defaults.model";
 import { propertyUpdateCondition } from "./property-update-condition.service";
+import { checkIfOverElement } from "./check-if-over-el.service";
+import { handleType } from "./handle-type.service";
 
 export let updateEl = (el, ev) => {
-    let elCenterPoint = getCenterPoint(el);
-    let elAngle = Math.atan2(ev.pageY - elCenterPoint.y, ev.pageX - elCenterPoint.x) * 180 / Math.PI;
-    let elAngleSegment = 180 / defaults.areas;
 
-    propertyUpdateCondition(el, elAngle, elAngleSegment);
+    const mousePoint = {
+        x: ev.pageX,
+        y: ev.pageY
+    };
+
+    const overEl = checkIfOverElement(el, mousePoint);
+
+    if ( overEl ) {
+        handleType(el, overEl);
+    } else if ( !overEl ) {
+        const elCenterPoint = getCenterPoint(el, overEl);
+        const mouseToElAngle = Math.atan2(ev.pageY - elCenterPoint.y, ev.pageX - elCenterPoint.x) * 180 / Math.PI;
+        propertyUpdateCondition(el, mouseToElAngle);
+    }
 }
