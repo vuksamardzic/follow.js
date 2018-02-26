65cf39f09d04b6555725dbbbb8b2bb1113d2fd6d
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-02-26 21:05:16 +0100

    upgrated calculation of mouse to element angle

--- src/services/property-update-condition.service.js
+++ src/services/property-update-condition.service.js
@@ -1,37 +1,20 @@
 import { defaults } from "../models/defaults.model";
+import { adjustWedgeAngles } from "./adjust-wedge-angles.service";
+import { handleType } from "./handle-type.service";
 
-let array = [
-    'right',
-    'top-right',
-    'top',
-    'top-left',
-    'left',
-    'bottom-left',
-    'bottom',
-    'bottom-right'
-];
-
-export let propertyUpdateCondition = (el, evAngle, elAngle) => {
-    let intervalI = elAngle;
-    let intervalII = elAngle - elAngle * 2;
+export let propertyUpdateCondition = (el, mouseToElAngle) => {
+    const part180 = 180 / defaults.areas;
+    const part360 = 360 / defaults.areas;
+    let wedgeAngleStart = part180;
+    let wedgeAngleEnd = -part180;
 
     for (let i = 0; i < defaults.areas; i++) {
-
-        if (!(intervalI < 0 && intervalII > 0)) {
-            if (evAngle < intervalI && evAngle > intervalII) {
-                el.innerHTML = array[i];
-            }
-        } else {
-            if (evAngle > intervalII || evAngle < intervalI) {
-                el.innerHTML = array[i];
-            }
+        if (wedgeAngleStart > mouseToElAngle && wedgeAngleEnd < mouseToElAngle) {
+            handleType(el, defaults.feed[i]);
+        } else if (wedgeAngleStart < 0 && wedgeAngleEnd > 0 && (mouseToElAngle > wedgeAngleEnd || mouseToElAngle < wedgeAngleStart)) {
+            handleType(el, defaults.feed[i]);
         }
 
-        intervalI = intervalII;
-        intervalII -= elAngle * 2;
-
-        if (intervalII < -180) {
-            intervalII = -intervalI;
-        }
+        [wedgeAngleStart, wedgeAngleEnd] = adjustWedgeAngles(wedgeAngleStart, wedgeAngleEnd, part360);
     }
 }
