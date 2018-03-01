954361960a93d7a70f5617754b8d8d989fb8a16d
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-01 11:27:17 +0100

    on load set elements to default state

--- src/services/merge-options.service.js
+++ src/services/merge-options.service.js
@@ -1,15 +1,25 @@
 import { defaults } from "../models/defaults.model";
 import { followEl } from "./cache-dom.service";
 import { followElArray } from "../models/follow-el.model";
+import { handleType } from "./handle-type.service";
 
 export let mergeOptions = (options) => {
     if (typeof options === 'string') {
         defaults.selector = options;
-        Array.from(followEl(defaults.selector)).map(i => followElArray.push(i));
+        Array.from(followEl(defaults.selector)).map(el => {
+            handleType(el, true);            
+            followElArray.push(el);
+        });
     } else if (typeof options === 'object') {
         Object.assign(defaults, options);
-        Array.from(followEl(defaults.selector)).map(i => followElArray.push(i)); 
+        Array.from(followEl(defaults.selector)).map(el => {
+            followElArray.push(el);
+            handleType(el, true);
+        });
     } else if (typeof options === undefined) {
-        Array.from(followEl(defaults.selector)).map(i => followElArray.push(i));
+        Array.from(followEl(defaults.selector)).map(el => {
+            handleType(el, true);
+            followElArray.push(el);
+        });
     }
 }
