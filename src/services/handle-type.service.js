9e3079617ab8d4256d8802dea5f5633e31d15f20
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-05 20:25:45 +0100

    handle type updated w/ new default obj

--- src/services/handle-type.service.js
+++ src/services/handle-type.service.js
@@ -1,24 +1,24 @@
-import { defaults } from "../models/defaults.model";
+import { settings } from "../models/settings.model";
 
 export let handleType = (el, val) => {
     if ( typeof val === 'boolean' ) {
-        if ( defaults.type === 'text' ) {
-            if ( el.innerText !== defaults.default ) {
-                el.innerText = defaults.default;
+        if ( settings.type === 'text' ) {
+            if ( el.innerText !== settings.default ) {
+                el.innerText = settings.default;
             }
-        } else if ( defaults.type === 'style' ) {
-            if ( el[defaults.type][defaults.prop] !== defaults.default ) {
-                el[defaults.type][defaults.prop] = defaults.default;
+        } else if ( settings.type === 'style' ) {
+            if ( el[settings.type][settings.prop] !== settings.default ) {
+                el[settings.type][settings.prop] = settings.default;
             }
         }
     } else {
-        if ( defaults.type === 'text' ) {
+        if ( settings.type === 'text' ) {
             if ( el.innerText !== val ) {
                 el.innerText = val;
             }
-        } else if ( defaults.type === 'style' ) {
-            if ( el[defaults.type][defaults.prop] !== val ) {
-                el[defaults.type][defaults.prop] = val;
+        } else if ( settings.type === 'style' ) {
+            if ( el[settings.type][settings.prop] !== val ) {
+                el[settings.type][settings.prop] = val;
             }
         }
     }
