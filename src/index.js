a441f07b4cc6169230e3f9726b1bbb6034044268
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-05 20:25:22 +0100

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { defaults } from './models/defaults.model';
-import { followEl } from './services/cache-dom.service';
 import { mouseMoveHandler } from './events/mousemove.event';
 import { mergeOptions } from './services/merge-options.service';
 
