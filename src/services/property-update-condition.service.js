319571a30dded7b330bf6d140c392c41e7370216
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-05 20:27:04 +0100

    new line at the end & settings obj update

--- src/services/property-update-condition.service.js
+++ src/services/property-update-condition.service.js
@@ -1,18 +1,18 @@
-import { defaults } from "../models/defaults.model";
+import { settings } from "../models/settings.model";
 import { adjustWedgeAngles } from "./adjust-wedge-angles.service";
 import { handleType } from "./handle-type.service";
 
 export let propertyUpdateCondition = (el, mouseToElAngle) => {
-    const part180 = 180 / defaults.areas;
-    const part360 = 360 / defaults.areas;
+    const part180 = 180 / settings.areas;
+    const part360 = 360 / settings.areas;
     let wedgeAngleStart = part180;
     let wedgeAngleEnd = -part180;
 
-    for (let i = 0; i < defaults.areas; i++) {
+    for (let i = 0; i < settings.areas; i++) {
         if (wedgeAngleStart > mouseToElAngle && wedgeAngleEnd < mouseToElAngle) {
-            handleType(el, defaults.feed[i]);
+            handleType(el, settings.feed[i]);
         } else if (wedgeAngleStart < 0 && wedgeAngleEnd > 0 && (mouseToElAngle > wedgeAngleEnd || mouseToElAngle < wedgeAngleStart)) {
-            handleType(el, defaults.feed[i]);
+            handleType(el, settings.feed[i]);
         }
 
         [wedgeAngleStart, wedgeAngleEnd] = adjustWedgeAngles(wedgeAngleStart, wedgeAngleEnd, part360);
