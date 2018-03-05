e3397502b7270f6caf4ca9f58ceb86153780ad08
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-05 20:27:46 +0100

    removed unused imports & small tweak

--- src/services/update-el.service.js
+++ src/services/update-el.service.js
@@ -1,23 +1,20 @@
 import { getCenterPoint } from "./el-center.service";
-import { defaults } from "../models/defaults.model";
 import { propertyUpdateCondition } from "./property-update-condition.service";
 import { checkIfOverElement } from "./check-if-over-el.service";
 import { handleType } from "./handle-type.service";
 
 export let updateEl = (el, ev) => {
-
-    const mousePoint = {
-        x: ev.pageX,
-        y: ev.pageY
+    const mouseCoordinates = {
+        x: ev.clientX,
+        y: ev.clientY
     };
-
-    const overEl = checkIfOverElement(el, mousePoint);
-
+    const overEl = checkIfOverElement(el, mouseCoordinates);
+    
     if ( overEl ) {
         handleType(el, overEl);
     } else if ( !overEl ) {
         const elCenterPoint = getCenterPoint(el, overEl);
-        const mouseToElAngle = Math.atan2(ev.pageY - elCenterPoint.y, ev.pageX - elCenterPoint.x) * 180 / Math.PI;
+        const mouseToElAngle = Math.atan2(mouseCoordinates.y - elCenterPoint.y, mouseCoordinates.x - elCenterPoint.x) * 180 / Math.PI;
         propertyUpdateCondition(el, mouseToElAngle);
     }
 }
